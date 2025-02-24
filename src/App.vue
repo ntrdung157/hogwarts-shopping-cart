7f2a9a515008c4c374746812bac58d47908c9b09
Dung Nguyen <dung.nguyen@tcu.edu>
2025-02-24 08:40:03 -0600

    episode 11

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,8 @@
                                 <input
                                 type="text" 
                                 class="quantity-input" 
-                                :value="shoppingCartItems[0].quantity" 
+                                v-model.number="shoppingCartItems[0].quantity"
                                 aria-label="quantity"
-                                @blur="changeQuantity(shoppingCartItems[0].id, $event)"
                                 >
                                 <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[0].id)">+</button>
                             </div>
@@ -47,9 +46,9 @@
                                 <input
                                 type="text" 
                                 class="quantity-input" 
-                                :value="shoppingCartItems[1].quantity" 
+                                v-model.number="shoppingCartItems[1].quantity"
                                 aria-label="quantity"
-                                @blur="changeQuantity(shoppingCartItems[1].id, $event)">
+                                >
                                 <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[1].id)">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -73,9 +72,9 @@
                                 <input
                                 type="text" 
                                 class="quantity-input" 
-                                :value="shoppingCartItems[2].quantity" 
+                                v-model.number="shoppingCartItems[2].quantity"
                                 aria-label="quantity"
-                                @blur="changeQuantity(shoppingCartItems[2].id, $event)">
+                                >
                                 <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[2].id)">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -99,9 +98,9 @@
                                 <input
                                 type="text" 
                                 class="quantity-input" 
-                                :value="shoppingCartItems[3].quantity" 
+                                v-model.number="shoppingCartItems[3].quantity"
                                 aria-label="quantity"
-                                @blur="changeQuantity(shoppingCartItems[3].id, $event)">
+                                >
                                 <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[3].id)">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -125,9 +124,9 @@
                                 <input
                                 type="text" 
                                 class="quantity-input" 
-                                :value="shoppingCartItems[4].quantity" 
+                                v-model.number="shoppingCartItems[4].quantity"
                                 aria-label="quantity"
-                                @blur="changeQuantity(shoppingCartItems[4].id, $event)">
+                                >
                                 <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[4].id)">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -227,13 +226,6 @@
         })
     }
 
-    function changeQuantity(id, event) {
-        shoppingCartItems.value.some(item => {
-            if (item.id == id) {
-                item.quantity = parseInt(event.target.value)
-            }
-        })
-    }
 </script>
 
 <style scoped>
